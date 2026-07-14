65295a679ef34a28d7342cd0c7db4111bd413196
M <6395323+mario-huang@users.noreply.github.com>
2026-07-14 16:19:50 +0900

    Update main.ts

--- src/main/main.ts
+++ src/main/main.ts
@@ -58,6 +58,14 @@ app.whenReady().then(() => {
   // startAria2()
   createWindow()
   // void readPreferences().then(applyLoginItemPreference)
+  
+  app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
@@ -66,14 +74,6 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
   }
 });
 
