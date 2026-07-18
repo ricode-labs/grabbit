5ce89e58e73d31a63d5544fdb8aa580f1671ce21
M <6395323+mario-huang@users.noreply.github.com>
2026-07-18 14:17:52 +0900

    Update window.ts

--- src/main/window.ts
+++ src/main/window.ts
@@ -29,10 +29,6 @@ export function showWindow() {
     mainWindow?.hide()
   })
 
-  mainWindow.on("closed", () => {
-    mainWindow = null
-  })
-
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
