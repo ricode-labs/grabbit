4983cf475fd87f918e8cd7a239fa6c41eadc187b
M <6395323+mario-huang@users.noreply.github.com>
2026-07-31 17:05:09 +0900

    hide frame

--- src/main/window.ts
+++ src/main/window.ts
@@ -14,6 +14,7 @@ export function showWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
     webPreferences: {
       preload: join(__dirname, "preload.js"),
     },
